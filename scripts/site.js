1bb10a87936d7a4cc5b29739f17c9ea2169dda95
Edwin Richards <edwin.richards@dearmansystems.com>
2019-12-08 23:40:47 -0700

    Initial Commit

---
+++ scripts/site.js
@@ -0,0 +1,52 @@
+$(document).ready(() => {  
+  // So we can know when elements are added (or removed) from the page
+  const target = document.querySelector('#page_content');
+  
+  let observer = new MutationObserver(() => observeIntersections());
+  
+  const config = {
+    childList: true,
+    subtree: true
+  };
+  
+  observer.observe(target, config);
+
+  // Load the home page info by default
+  loadContent('home.html');
+
+  // When a nav item is clicked, load the corresponding content
+  $('.nav-item').click(function() {
+    loadContent($(this).attr('data-name'));
+  });
+});
+
+// Retrieves and loads the content for the page
+loadContent = (page) => {
+  $('#page_content').hide().load(page).fadeIn('1500');
+
+  // Set the title of the page based on the content shown
+  document.title = page == 'about.html' ? 'About Edwin Richards' :
+    page == 'projects.html' ? 'Personal Projects' :
+    page == 'resume.html' ? 'Resumé' :
+    page == 'contact.html' ? 'Contact Edwin Richards' : 'Edwin Richards | Full-Stack Software Developer';
+}
+
+// Sets up the intersection observer to handle animating when elements are in view
+observeIntersections = () => {
+  let target = document.querySelector('.lazy-fader');
+
+  if (target != null) {
+    let observer = new IntersectionObserver(intersectCallback);
+
+    observer.observe(target);
+  }
+}
+
+// Callback function called when an intersection is observed
+intersectCallback = (entries, obs) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0.1) {
+      $('.lazy-fader').fadeIn(1000);
+    }
+  });
+}
