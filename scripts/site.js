dc732f662a28350a4ee62df30a5f3343754f4955
Edwin Richards <edwin.richards@dearmansystems.com>
2019-12-27 00:20:30 -0700

    Add contact form and validation

--- scripts/site.js
+++ scripts/site.js
@@ -5,6 +5,19 @@ $(document).ready(() => {
 
   observeIntersections();
   typeCode();
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = $('.needs-validation');
+  // Loop over them and prevent submission
+  Array.prototype.filter.call(forms, (form) => {
+    form.addEventListener('submit', (event) => {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
 });
 
 updateDebugState = () => {
