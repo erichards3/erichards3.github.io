612e037e2357491b981777ebc98b9079445e0545
Eddie Richards <grnbypckrs@gmail.com>
2026-01-23 18:47:07 -0700

    Update in prep for rework for new site

--- scripts/site.js
+++ scripts/site.js
@@ -1,21 +1,3 @@
-const contactPlaceholders = [
-  {'name': 'Jon Snow', 'email': 'jon.snow@nightswatch.com', 'message': 'My watch has ended!'},
-  {'name': 'Samwell Tarley', 'email': 'sam.tarly@nightswatch.com', 'message': 'You\'re better than me in everything. Except reading.'},
-  {'name': 'Daenerys Targaryen', 'email': 'motherofdragons@danyforqueen.org', 'message': 'Bend the knee... or DIE!'},
-  {'name': 'Tyrion Lannister', 'email': 'tinylannister@danyforqueen.org', 'message': 'That\'s what I do. I drink and I know things.'},
-  {'name': 'Petyr Baelish', 'email': 'little.finger@kingscouncil.com', 'message': 'Money buys a man\'s silence for a time. A bolt in the heart buys it forever.'},
-  {'name': 'Arya Stark', 'email': 'arya@thefaceless.org', 'message': 'A girl has no name.'},
-  {'name': 'Brienne of Tarth', 'email': 'brienne@newknights.com', 'message': 'I, Brienne of Tarth, sentence you to die. Do you have any last words?'},
-  {'name': 'Bran Stark', 'email': 'bran.stark@teamwarg.com', 'message': 'How do you know there is an afterwards?'},
-  {'name': 'Sansa Stark', 'email': 'sansa.stark@downwithdany.org', 'message': 'Definitely NOT getting married again.'},
-  {'name': 'Cersei Lannister', 'email': 'cersei@ruthlessrulers.com', 'message': 'When you play the Game of Thrones, you win or you die. There is no middle ground.'},
-  {'name': 'Jaime Lannister', 'email': 'jamie.lannister@kingsguard.com', 'message': 'Would you lend me a hand? I seem to have lost mine.'},
-  {'name': 'Tormund Giantsbane', 'email': 'tormund@giantsmilk.sales', 'message': 'Is the big woman still here?'},
-  {'name': 'Ygritte', 'email': 'ygritte@toparchers.org', 'message': 'You know nothing, Jon Snow.'},
-  {'name': 'Melisandre', 'email': 'melisandre@lordoflight.org', 'message': 'The night is dark and full of terrors.'},
-  {'name': 'Hodor', 'email': 'hodor@hodor.com', 'message': 'Hodor hodor hodor, hodor hodor Hodor hodor.'},
-]
-
 $(document).ready(() => {
   observeIntersections();
   typeCode();
@@ -32,12 +14,6 @@ $(document).ready(() => {
       form.classList.add('was-validated');
     }, false);  
   });  
-
-  formPlaceholder = contactPlaceholders[randomInteger(0, contactPlaceholders.length)];
-
-  $('#contact_name').attr('placeholder', formPlaceholder['name']);
-  $('#contact_email').attr('placeholder', formPlaceholder['email']);
-  $('#contact_message').attr('placeholder', formPlaceholder['message']);
 });  
 
 // Sets up the intersection observer to handle animating when elements are in view
